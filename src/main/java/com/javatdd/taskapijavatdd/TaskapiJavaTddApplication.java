6a1805be7f3c6e4d6a94fbe6304d14aae5e18319
Ashfaq Hussain Ahmed <ashfaqshuvo14@gmail.com>
2024-07-15 13:22:06 +0300

    Project bootstrapping

---
+++ src/main/java/com/javatdd/taskapijavatdd/TaskapiJavaTddApplication.java
@@ -0,0 +1,13 @@
+package com.javatdd.taskapijavatdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskapiJavaTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskapiJavaTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatdd/taskapijavatdd/TaskapiJavaTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatdd.taskapijavatdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskapiJavaTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
